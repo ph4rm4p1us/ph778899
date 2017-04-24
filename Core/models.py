91b441e46771170b01dd7c527e927ed4e8937897
Fouad Fawzi <fouadfawzi@gmail.com>
2017-04-24 22:11:27 +0200

    work on core

--- Core/models.py
+++ Core/models.py
@@ -2,21 +2,22 @@ from django.db import models
 from django.contrib.auth.models import Permission, User
 
 # Create your models here.
+'''
 class Pharmacy(models.Model):
     owner = models.ForeignKey(User, unique=True)
     name = models.CharField(max_length=64)
     def __str__(self):
         return self.name
 
-class Pharmacy_Employee(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    pharmacy = models.ForeignKey(Pharmacy)
-    phone = models.CharField(max_length=15)
-    national_id = models.IntegerField(max_length=14)
+'''
 
-class Medicine_DB(models.Model):
-    int_barcode = models.IntegerField()
-    
+
+class CoreMedicine(models.Model):
+    barcode = models.CharField(max_length=32)
+    comerical_name = models.CharField(max_length=200)
+    arabic_name = models.CharField(max_length=200)
+
+
 
 
 
